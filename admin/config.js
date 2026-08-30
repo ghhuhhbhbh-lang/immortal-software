fac92819b8c73bec0680e54719d85e0755720853
Immortal Pages <pages@immortal.local>
2026-08-30 23:58:26 +0100

    Skip portal gate; land on admin usr/pass login

--- admin/config.js
+++ admin/config.js
@@ -1,5 +1,3 @@
 ﻿(function (w) {
   w.IMMORTAL_API = 'https://liquid-workflow-surgical-liz.trycloudflare.com';
-  w.IMMORTAL_PORTAL = w.IMMORTAL_PORTAL || new URL('../', location.href).href;
-  w.IMMORTAL_LOADER = w.IMMORTAL_LOADER || new URL('../loader/', location.href).href;
 })(window);
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 ﻿(function (w) {
   w.IMMORTAL_API = 'https://liquid-workflow-surgical-liz.trycloudflare.com';
-  w.IMMORTAL_LOADER = w.IMMORTAL_LOADER || new URL('loader/', location.href).href;
-  w.IMMORTAL_ADMIN = w.IMMORTAL_ADMIN || new URL('admin/', location.href).href;
+  w.IMMORTAL_LOADER = new URL('loader/', location.href).href;
+  w.IMMORTAL_ADMIN = new URL('admin/', location.href).href;
 })(window);
